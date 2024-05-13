90eb23f558ccd8f6159d175e350f295b51547cca
Jundi Abdullah <jdi.jundi99@gmail.com>
2024-05-14 05:55:05 +0700

    merapikan file create person dari code sebelumnya dalam hal fungsi menambah tag/subject

--- resources/views/admin/people/create.blade.php
+++ resources/views/admin/people/create.blade.php
@@ -1,17 +1,3 @@
-{{-- <script>
-    const {
-        JSDOM
-    } = require('jsdom');
-    const {
-        window
-    } = new JSDOM();
-    const {
-        document
-    } = new JSDOM('').window;
-    global.document = document;
-    const $ = require('jquery')(window);
-</script> --}}
-
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -140,41 +126,8 @@
                         <x-input-error :messages="$errors->get('status')" class="mt-2" />
                     </div>
 
-                    {{-- <div class="mt-4">
-                        <x-input-label for="subjects" :value="__('Subject Areas Expertise')" />
-                        <p class="my-1 text-xs text-slate-400">pisahkan dengan coma</p>
-                        <x-text-input id="subjects" class="block mt-1 w-full" type="text" subjects="subjects"
-                            name="subjects" :value="old('subjects')" autofocus autocomplete="subjects" />
-                        <x-input-error :messages="$errors->get('subjects')" class="mt-2" />
-                    </div>
- --}}
-
-                    {{-- <div class="mt-4">
-                        <x-input-label for="file" :value="__('Subject Areas Person')" />
-                        <p class="my-1 text-xs text-slate-400">pisahkan dengan coma</p>
-                        <div class="relative w-full">
-                            <input type="hidden" class="form-control" name="subjs" id="perSubjs">
-                            <input type="text" id="name-subject" name="subjects"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 "
-                                placeholder="Subject Areas" :value="old('subjects')" autofocus
-                                autocomplete="subjects" />
-                            <a id="btn-subj"
-                                class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
-                    <div>
-                        <div id="subjPer"></div>
-                    </div> --}}
-
-
                     <div class="mt-4" x-data="{ data: [], subject: {} }">
                         <x-input-label for="file" :value="__('Subject Areas Person')" />
-                        {{-- <p class="my-1 text-xs text-slate-400">pisahkan dengan coma</p> --}}
                         <div class="relative w-full">
                             <input type="text" id="name-subject" name="subjects" x-model="subject"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 "
@@ -226,77 +179,9 @@
 
     <script src="https://cdn.ckeditor.com/4.22.1/standard/ckeditor.js"></script>
 
-    {{-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script> --}}
     <script>
         CKEDITOR.replace('project');
         CKEDITOR.replace('publication');
         CKEDITOR.replace('hki');
     </script>
-    {{-- <script>
-        const subjs = []
-        const perSubj = []
-
-        document.getElementById("btn-subj").onclick = function() {
-
-            const namesubjet = document.getElementById("name-subject").value
-            const subjPer = document.getElementById("subjPer")
-            const perSubjs = document.getElementById("perSubjs")
-
-            let newSubj =
-                ' <div class="inline-flex mt-3 items-center h-8 overflow-hidden text-white bg-blue-500 rounded">  <span class="px-5 py-1.5 text-[12px] font-medium">' +
-                namesubjet +
-                '</span> </div>'
-
-            perSubj.push(namesubjet)
-            subjs.push(newSubj)
-
-            subjPer.innerHTML = subjs.join(' ')
-            console.log(perSubj)
-            perSubjs.value = perSubj
-
-        }
-    </script> --}}
-    {{-- <script>
-        const subjs = []
-        const perSubj = []
-
-        const namesubjet = document.getElementById("name-subject")
-        const subjPer = document.getElementById("subjPer")
-        const perSubjs = document.getElementById("perSubjs").value
-
-        eve();
-
-        function eve() {
-            namesubjet.addEventListener('keyup', function(e) {
-                const namesubjet = document.getElementById("name-subject").value
-                const subjPer = document.getElementById("subjPer")
-                const perSubjs = document.getElementById("perSubjs")
-
-                let newSubj =
-                ' <div class="inline-flex mt-3 items-center h-8 overflow-hidden text-white bg-blue-500 rounded">  <span class="px-5 py-1.5 text-[12px] font-medium">' +
-                namesubjet + '</span> <button onclick="return this.parentNode.remove();"  id="removeSubj' + i +
-                '"  class="inline-flex items-center justify-center w-8 h-8 bg-blue-600 transition-color hover:bg-blue-700 focus:outline-none focus:ring" type="button"  >    <span class="sr-only" > Close </span>    <svg  class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"> <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />    </svg>  </button> </div>'    
-                
-                // perSubj.push(namesubjet)
-                // subjs.push(newSubj)
-
-                subjPer.innerHTML = subjs
-
-                if (e.keyCode === 188) {
-                    subjs.push(newSubj)
-                    console.log(subjs)
-                }
-            })
-        }
-
-        // let newSubj =
-        //     ' <div class="inline-flex mt-3 items-center h-8 overflow-hidden text-white bg-blue-500 rounded">  <span class="px-5 py-1.5 text-[12px] font-medium">' +
-        //     cm +
-        //     '</span> <button onclick="return this.parentNode.remove();"  id="removeSubj"  class="inline-flex items-center justify-center w-8 h-8 bg-blue-600 transition-color hover:bg-blue-700 focus:outline-none focus:ring" type="button"  >    <span class="sr-only" > Close </span>    <svg  class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"> <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />    </svg>  </button> </div>'
-        // console.log(newSubj)
-
-        // function cm(newSubj) {
-        //     subjPer.innerText = newSubj.target.value
-        // }
-    </script> --}}
 </x-app-layout>
